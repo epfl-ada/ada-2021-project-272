fa30807ba7e31808fe592bd6ae7dde775b8b310a
npaulish <91085728+npaulish@users.noreply.github.com>
2021-11-12 17:29:42 +0100

    Added textual description

--- preprocessing.py
+++ preprocessing.py
@@ -3,10 +3,17 @@ import numpy as np
 
 
 def delete_non_complete_entries(chunk):
+    '''
+    The function replaces 'None' values in the chunk by numpy NaN and deletes entries with missing quotation or speaker.
+    It returns the modified chunk.
+    '''
     chunk = chunk.replace('None', np.NaN)
     chunk = chunk.dropna(subset=['quotation', 'speaker'])
     return chunk
 
 def delete_duplicates(chunk):
+    '''
+    The function deletes duplicates in the chunk and returns the modified chunk.
+    '''
     chunk = chunk.drop_duplicates(subset=['quoteID','speaker', 'date'])
     return chunk
